f0cf2d390779bab73b57b6e3e1ca41f857f7e35d
Maisam Bakhshi <maisambakhshi@Maisams-MacBook-Pro.local>
2024-04-28 16:13:51 +1000

    data added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { PostComponent } from './common/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/post/post.model.ts
@@ -0,0 +1,8 @@
+export interface Post {
+    id: number;
+    img: string;
+    title: string;
+    body: string;
+    date: string;
+    user: string;
+}
